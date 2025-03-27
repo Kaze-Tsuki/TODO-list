56017b5eb7ebfe8055f8d0fb881da19f7a0fe71a
風月春秋 <seancai78@gmail.com>
2025-03-27 17:20:26 +0800

    first edition

---
+++ README.md
@@ -0,0 +1,73 @@
+# Instruction of todo list
+
+## test cases
+
+<details>
+<summary>basic add/del/view</summary>
+
+```
+build_li First
+add First task1 test 0
+add First task2 test 1
+add First task3 test 0
+prall First
+swId First 2 3
+add First new nothing 0
+prall First
+setComp First 4 1
+sort First name
+prall First
+sort First cate
+prall First
+sort First comp 1
+prall First
+sort First name 0
+prall First
+filter First test
+prall First-test
+rm First-test cate test
+rm First name new
+rm First id 4
+prall First
+prall First-test
+rm First list
+add First task again 1
+add First-test task again 1
+prall First-test
+```
+</details>
+
+<details>
+<summary>basic copy/edit</summary>
+
+```
+build_li a
+add a new task 1
+cpy a b
+prall b
+chg b 1 name newname
+chg b 1 category newtask
+chg b 1 comp 0
+chgli b newb
+prall newb
+```
+</details>
+
+<details>
+<summary>Advanced merge/intersect</summary>
+
+```
+build_li a
+add a task1 norm 1
+add a task1 norm 0
+add a task2 emer 1
+cpy a b
+add b task no 0
+merge a b merged
+inter a b intersect
+prall a
+prall b
+prall merged
+prall intersect
+```
+</details>
---
+++ dependency.h
@@ -0,0 +1,59 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class norm_task
+{
+protected:
+    string *name;
+    string *category;
+    bool *completed;
+public:
+    norm_task(string name, string category, bool completed);
+    norm_task(const norm_task &t);
+    virtual void print();
+    virtual void change_completed(bool ncompleted);
+    virtual void change_name(string name);
+    virtual void change_category(string category);
+    virtual bool get_completed();
+    virtual string get_name();
+    virtual string get_category();
+    
+    bool operator<(norm_task &t);
+    bool operator>(norm_task &t);
+    bool operator==(norm_task &t);
+    bool operator!=(norm_task &t);
+    friend ostream &operator<<(ostream &os, norm_task &t);
+    virtual ~norm_task();
+};
+
+class list
+{
+protected:
+    vector <norm_task*> tasks;
+    string* name;
+public:
+    list();
+    list(string* name);
+    list(const list &l);
+    list(string* name, const list &l);
+    string& get_name();
+    void change_name(string* name);
+    void add_task(norm_task *task);
+    norm_task* get_task(int index);
+    void printAll();
+    void printtask(int index);
+    void switch_id(int index1, int index2);
+    void rm_taskWid(int index);
+    void rm_taskWname(string name);
+    void rm_taskWcate(string category);
+    void sort(const string type, const bool ascending);
+    list filter(const string category);
+    void clear();
+    list operator+(list &l);
+    list operator&(list &l);
+
+    ~list();
+};
